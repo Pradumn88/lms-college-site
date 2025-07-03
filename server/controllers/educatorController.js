fdde4386f6d80d948bb04b0d4da141ee0f28738c
Pradumn88 <ishup3658@gmail.com>
2025-07-03 08:14:49 +0530

    stripe added

--- server/controllers/educatorController.js
+++ server/controllers/educatorController.js
@@ -44,3 +44,80 @@ export const addCourse = async (req, res)=>{
         res.json({ success: false, message: error.message})
     }
 }
+
+// get controller function
+export const  getEducatorCourses = async (req,res)=>{
+    try {
+        const educator = req.auth.userId
+
+        const courses = await Course.find({educator})
+        res.json({success: true, courses})
+    } catch (error) {
+        res.json({success: false, message: error.message})
+    }
+}
+
+//get educator dashboard data (total earning,enrolled students ,no. of courses)
+
+export const educatorDashboardData = async (req, res)=>{
+    try {
+        const educator = req.auth.userId;
+        const courses  = await Course.find({educator});
+        const totalCourses = courses.length;
+
+        const courseIds =  courses.map(course => course._id);
+
+        //calculate total earnings
+        const purchases =  await Purchase.find({
+            courseId: {$in: courseIds},
+            status: 'completed'
+        });
+        
+        const totalEarnings = purchases.reduce((sum, purchase)=> sum+purchase.amount, 0);
+
+        //collect uhniqueid enrolled student id with their course titles
+        const enrolledStudentsData = [];
+        for(const course of courses){
+            const students = await User.find({
+                _id: {$in: course.enrolledStudents}
+            },'name imageUrl');
+            students.forEach(student => {
+                enrolledStudentsData.push({
+                    courseTitle: course.courseTitle,
+                    student
+                });
+            });
+        }
+
+        res.json({success: true,dashboardData: {
+            totalEarnings, enrolledStudentsData,totalCourses
+        }})
+
+    } catch (error) {
+        res.json({success: false, message: error.message});
+    }
+}
+
+// get enrolled students data with purchase data
+
+export const getEnrolledStudentsData = async(req, res)=>{
+    try {
+        const educator = req.auth.userId;
+        const courses  = await Course.find({educator});
+        const courseIds =  courses.map(course => course._id);
+
+        const purchases = await Purchase.find({
+            courseId: { $in: courseIds},
+            status: 'completed'
+        }).populate('userId', 'name imageUrl').populate('courseId', 'courseTitle')
+
+        const enrolledStudents = purchases.map(purchase => ({
+            student: purchase.userId,
+            courseTitle: purchase.courseId.courseTitle,
+            purchaseDate: purchase.createdAt
+        }));
+        res.json({success: true, enrolledStudents});
+    } catch (error) {
+        res.json({success: false, message: error.message});
+    }
+}
